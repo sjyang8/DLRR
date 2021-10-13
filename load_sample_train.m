47dc4d42fdded3d5f4b82ac3aa86e812635296b3
sjyang8 <xingguinvwu@gmail.com>
2021-10-13 21:49:30 +0800

    Update load_sample_train.m

--- load_sample_train.m
+++ load_sample_train.m
@@ -1,5 +1,9 @@
 function [loc_train, loc_test, CTest] = load_sample_train(img_name,per_ratio)
-    load_path=['./data/sample_train/'];
+% load the sample train and test index
+%---------------------------------------------
+%written by Shujun Yang (yangsj3@sustech.edu.cn; sjyang8-c@my.cityu.edu.hk)
+%---------------------------------------------
+	load_path=['./data/sample_train/'];
 	file_name=[img_name num2str(per_ratio) '_sample.mat'];
 	load([load_path file_name],'loc_train', 'loc_test', 'CTest');
 end
